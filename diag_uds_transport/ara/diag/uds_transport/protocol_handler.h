3cf0532d77f0d07238b0fd4503b14fb057230ee7
Yiyang-CYY <yiyang.cai@pm.me>
2022-11-01 15:38:27 +0800

    update UdsTransportProtocolHandler class.

--- diag_uds_transport/ara/diag/uds_transport/protocol_handler.h
+++ diag_uds_transport/ara/diag/uds_transport/protocol_handler.h
@@ -9,6 +9,7 @@
 
 #include "protocol_mgr.h"
 #include "protocol_types.h"
+#include "uds_message.h"
 
 namespace ara {
 namespace diag {
@@ -47,16 +48,51 @@ public:
    */
   inline UdsTransportProtocolHandler(
       UdsTransportProtocolHandlerID const handlerId,
-      UdsTransportProtocolMgr &transportProtocolMgr)
-      : transportProtocolMgr_{transportProtocolMgr}, handlerId_{handlerId} {}
+      UdsTransportProtocolMgr &transportProtocolMgr);
 
-/**
- * @brief  Initializes handler. Must be called before Start(). The idea is to
- *         have "initialization" of handler-plugin separated from its ctor.
- * @return InitializationResult
- * @attention SWS_DM_00319
- */
+  /**
+   * @brief: Destructor of UdsTransportProtocolHandler
+   * @attention: SWS_DM_09016
+   */
+  virtual ~UdsTransportProtocolHandler();
+
+  /**
+   * @brief: Return the UdsTransportProtocolHandlerId, which was given to the
+   *         implementation during construction.
+   * @return UdsTransportProtocolHandlerID
+   * @attention: SWS_DM_00325
+   */
+  virtual UdsTransportProtocolHandlerID GetHandlerId() const;
+
+ /**
+  * @brief  Initializes handler.
+  *        Must be called before Start(). The idea is to have "initialization"
+  *        of handler-plugin separated from its ctor.
+  * @return InitializationResult
+  * @attention SWS_DM_00319
+  */
   virtual InitializationResult Initialize() = 0;
+
+  /**
+   * @brief: Tells the UdsTransportProtocolHandler, that it shall notify the DM
+   *         core via UdsTransportProtocolMgr::ChannelReestablished() if the
+   *         given channel has been re-established after next UdsTransportProtocolHandler::Start().
+   *         The main purpose of this method is to allow DM to provide an
+   *         ECU-Reset(0x11 service), with configuration option "Pos. response
+   *         AFTER reset". In this scenario the request of 0x11 will be received
+   *         on a certain channel with identifying tuple<p_x, p_y>(GlobalChannelIdentifier)
+   *         Then the ECU-Reset takes place and after ECU-Reset all plugins/
+   *         UdsTransportProtocolHandlers get restarted via call to Start().
+   *         Now there are two exceptions, when this method has been called
+   *         before and return "TRUE": IF the same remote client connects to
+   *         the UdsProtocolHandler, it shall get a channel identification with
+   *         the same identifying tuple<p_x, c_y> as last time. It shall call
+   *         this method.
+   * @param channelId
+   * @return
+   * @attention: SWS_DM_00326
+   */
+  virtual bool NotifyReestablishment(ChannelID channelId) = 0;
 
   /**
    * @brief Start Processing the implemented Uds Transport Protocol.
@@ -80,11 +116,26 @@ public:
    */
   virtual void Stop() = 0;
 
-protected:
-  UdsTransportProtocolMgr &transportProtocolMgr_;
+  /**
+   * @brief: Transmit a UDS message via the underlying UDS Protocol channel.
+   *         This transmit API covers T_Data.req of ISO 14229-2 Figure 2.
+   * @param message  : The message to be transmitted as a UdsMessage::Ptr.
+   *                   UdsTransportProtocolHandler has to give back this Ptr
+   *                   via UdsTransportProtocolMgr::TransmitConfirmation() to
+   *                   signal, that it is done with this message.
+   * @param channelId: Identification of channel on which to transmit.
+   * @attention: SWS_DM_00327
+   */
+  virtual void Transmit(UdsMessageConstPtr message, ChannelID channelId) = 0;
 
-private:
-  UdsTransportProtocolHandlerID const handlerId_;
+  /**
+   * @brief: Return corresponding periodic TP handler.
+   * @return ara::core::Result<UdsTransportProtocolHandler&>:
+   *         UdsTransportProtocolPeriodicHandler reference if periodic transmissions
+   *         are supported on this transport protocol, an error if not supported.
+   * @attention: SWS_DM_01068
+   */
+  virtual ara::core::Result<UdsTransportProtocolHandler&> GetPeriodicHandler();
 };
 } // namespace uds_transport
 } // namespace diag
