fa6649acef8ae93cac5ce11a4dee127fdfeffefc
Yiyang-CYY <yiyang.cai@pm.me>
2022-11-09 22:59:49 +0800

    daily commit

---
+++ diag_uds_transport/ara/diag/generic_data_identifier.h
@@ -0,0 +1,20 @@
+/***
+*  Author: yiyang.cai@pm.me
+*  DOC   : AUTOSAR_SWS_Diagnostics R21-11
+*  Date  : 11.9.2022
+ */
+
+#ifndef CYY_AP_GENERIC_DATA_IDENTIFIER_H
+#define CYY_AP_GENERIC_DATA_IDENTIFIER_H
+
+/**
+ * @brief: Generic DataIdentifier interface
+ * @attention： SWS_DM_00607
+ */
+class GenericDataIdentifier {
+
+
+};
+
+
+#endif // CYY_AP_GENERIC_DATA_IDENTIFIER_H
