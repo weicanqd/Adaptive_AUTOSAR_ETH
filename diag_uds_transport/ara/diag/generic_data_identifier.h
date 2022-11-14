b969a0d1bc80359f76474cf32db5ec3a9f1f6a47
Yiyang-CYY <yiyang.cai@pm.me>
2022-11-14 11:54:23 +0800

    daily commit

--- diag_uds_transport/ara/diag/generic_data_identifier.h
+++ diag_uds_transport/ara/diag/generic_data_identifier.h
@@ -7,6 +7,9 @@
 #ifndef CYY_AP_GENERIC_DATA_IDENTIFIER_H
 #define CYY_AP_GENERIC_DATA_IDENTIFIER_H
 
+#include <cstdint>
+#include "meta_info.h"
+
 /**
  * @brief: Generic DataIdentifier interface
  * @attention： SWS_DM_00607
@@ -29,6 +32,44 @@ class GenericDataIdentifier {
    */
   inline GenericDataIdentifier (const ara::core::InstanceSpecifier &specifier,
                                 DataIdentifierReentrancyType reentrancyType);
+
+  /**
+   * @brief: Destructor of class.
+   * @attention: SWS_DM_00635.
+   */
+  virtual ~GenericDataIdentifier() noexcept=default;
+
+  /**
+   * @brief: This Offer will enable the DM to forward request messages to this handler.
+   * @attention: SWS_DM_00638
+   */
+  ara::core::Result<void> Offer();
+
+  /**
+   * @brief: This StopOffer will disable the forwarding of request messages from DM.
+   * @attention: SWS_DM_00639
+   */
+  void StopOffer();
+
+  /**
+   * @brief: Called for ReadDataByIdentifier request for this DiagnosticDataIdentifier.
+   * @param dataIdentifier : the corresponding DataIdentifier
+   * @param metaInfo : contains additional meta information
+   * @param cancellationHandler : informs if the current conversation is canceled
+   * @attention: SWS_DM_00636
+   */
+  virtual ara::core::Future<OperationOutput> Read(std::uint16_t dataIdentifier,
+                                                  MetaInfo &metaInfo,
+                                                  CancellationHandler cancellationHandler) = 0;
+
+  /**
+   * @brief: Called for WriteDataByIdentifier request for this DiagnosticDataIdentifier.
+   * @param dataIdentifier : the corresponding DataIdentifier
+   * @param cancellationHandler : informs if the current conversation is canceled
+   * @attention: SWS_DM_00637
+   */
+  virtual ara::core::Future<void> Write(std::uint16_t dataIdentifier,
+                                        CancellationHandler cancellationHandler) = 0;
 };
 
 
